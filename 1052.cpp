c7ee7774d2f7adea9054f85ecd540754ff7e10c1
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-02-23 23:45:28 -0300

    New: 1052

---
+++ 1052.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x;
+
+    cin >> x;
+
+    if ( x == 1) {
+        cout << "January" << endl;
+    }
+
+    if ( x == 2) {
+        cout << "February" << endl;
+    }
+
+    if ( x == 3) {
+        cout << "March" << endl;
+    }
+
+    if ( x == 4) {
+        cout << "April" << endl;
+    }
+
+    if ( x == 5) {
+        cout << "May" << endl;
+    }
+
+    if ( x == 6) {
+        cout << "June" << endl;
+    }
+
+    if ( x == 7) {
+        cout << "July" << endl;
+    }
+
+    if ( x == 8) {
+        cout << "August" << endl;
+    }
+
+    if ( x == 9) {
+        cout << "September" << endl;
+    }
+
+    if ( x == 10) {
+        cout << "October" << endl;
+    }
+
+    if ( x == 11) {
+        cout << "November" << endl;
+    }
+
+    if ( x == 12) {
+        cout << "December" << endl;
+    }
+
+return 0;
+}
