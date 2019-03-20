1d54f19efd00f8d68561d89ebe9855b2344c20aa
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-03-19 23:39:08 -0300

    New: 1078

---
+++ 1078.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "1 x " << n << " = " << n * 1<< endl;
+    cout << "2 x " << n << " = " << n * 2<< endl;
+    cout << "3 x " << n << " = " << n *  3<< endl;
+    cout << "4 x " << n << " = " << n * 4<< endl;
+    cout << "5 x " << n << " = " << n * 5<< endl;
+    cout << "6 x " << n << " = " << n * 6<< endl;
+    cout << "7 x " << n << " = " << n * 7<< endl;
+    cout << "8 x " << n << " = " << n * 8<< endl;
+    cout << "9 x " << n << " = " << n * 9<< endl;
+    cout << "10 x " << n << " = " << n * 10<< endl;
+
+    return 0;
+}
