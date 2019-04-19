387cbb8e190ecfc9a79a2746ea30bf9a6f64cea6
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-04-19 10:03:00 -0300

    New: 1133

---
+++ 1133.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y, aux;
+    cin >> x >> y;
+
+    if(x > y) {
+        aux = x;
+        x = y;
+        y = aux;
+    }
+
+    for(int i=x+1; i<y; i++) {
+        if(i%5==2 or i%5==3)
+            cout << i << endl;
+    }
+
+    return 0;
+}
